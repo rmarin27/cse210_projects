16dd02a9fadb2c83c303a77a82954a2a1ed65d85
rmarin27 <rachelmarin27@gmail.com>
2024-02-27 12:55:48 -0700

    letter grade thangs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("what is your grade percentage? ");
+        string text = Console.ReadLine();
+        int percentage = int.Parse(text);
+        string letter = "";
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)  
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}.");
+        if (percentage >= 70)
+        {
+            Console.Write("Congradulations, you passed the class!");
+        }
+        else 
+        {
+            Console.Write("Better luck next time, you failed the class :D");
+        }
+
+
     }
 }
